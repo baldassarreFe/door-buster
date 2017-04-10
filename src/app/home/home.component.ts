cacc35ab8dbd3676b3af42e400b4db17694e9c4a
Federico Baldassarre <baldassarre.fe@gmail.com>
2017-04-10 16:13:37 +0200

    Simplified final status handling in application form and fixed margins in nav icons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
   template: `
-<navigation title="Door Buster!" [buttons]="[{icon:'account_circle', link:'/profile'}, {icon:'note_add', link:'/editor'}]"></navigation>
-<dashboard></dashboard>
-`
+    <navigation title="Door Buster!" [buttons]="icons"></navigation>
+    <dashboard></dashboard>
+  `
 })
 export class HomeComponent {
+  public icons = [{icon: 'note_add', link: '/editor'}, {icon: 'account_circle', link: '/profile'}];
 }
--- src/app/shared/navigation/nav-link.component.ts
+++ src/app/shared/navigation/nav-link.component.ts
@@ -1,26 +1,29 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input} from "@angular/core";
 
 @Component({
   selector: 'nav-icon-link',
   template: `
-<ng-container *ngIf="link">
-  <a [routerLink]="link" routerLinkActive="active">
-    <md-icon class="icon">{{icon}}</md-icon>
-  </a>
-</ng-container>
-<ng-container *ngIf="action">
+    <ng-container *ngIf="link">
+      <a [routerLink]="link" routerLinkActive="active">
+        <md-icon class="icon">{{icon}}</md-icon>
+      </a>
+    </ng-container>
+    <ng-container *ngIf="action">
   <span style="cursor: pointer" (click)="action()">
     <md-icon class="icon">{{icon}}</md-icon>
   </span>
-</ng-container>
-`,
+    </ng-container>
+  `,
   styles: [`
-.icon {
-  padding: 0 14px;
-  color: white;
-  text-decoration: none;
-}
-`]
+    span, a {
+      margin-left: 5px;
+    }
+  `, `
+    .icon {
+      color: white;
+      text-decoration: none;
+    }
+  `]
 })
 export class NavLinkComponent {
 
